be9e38e1abae10053259c7208f0a98e44c1a7d7d
Gutierrez Jesica <jesica89@gmail.com>
2025-08-13 15:50:42 -0300

    condicionales de de js

--- index.php
+++ index.php
@@ -6,9 +6,27 @@
 </head>
 <body>
     <?php
+
+    if(!isset($_GET['vista']) || $_GET['vista'] == "") {
+        $_GET['vista']="login";
+    }
+
+    if(is_file("./vistas/".$GET_['vista'].".php") && $_GET['vista']
+    !="login" && $_GET['vista']!="404") {
+
     include './inc/navbar.php';
     
+    include './vistas/".$_GET[vista].".php';
+
     include './inc/script.php';
+
+    }else{
+        if($_GET['vista']=="login") {
+            include './vistas/login.php';
+        }else {
+            include './vistas/404.php';
+        }
+    }
     ?>
 
 </body>
