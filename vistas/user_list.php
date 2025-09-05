e95ebbd5c0537d24a5d6c7b33d0469e3259c35ef
Jesica Gutierrez <jesica89@gmail.com>
2025-09-05 11:01:18 -0300

    mostrar usuarios de la lista

--- vistas/user_list.php
+++ vistas/user_list.php
@@ -1,1 +1,45 @@
-Nueva lista
+<div class="container is-fluid mb-6">
+    <h1 class="title">Usuarios</h1>
+    <h2 class="subtitle">Lista de usuarios</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+
+    <?php # INCORPORACIÓN DE ARCHIVOS PRINCIPALES PARA CARGAR LA LISTA DE USARIOS #
+        require_once "./php/main.php";
+
+        if(!isset($_GET['page'])) {
+            $pagina=1;
+        }else {
+            $pagina=(int)$_GET['page'];
+            if($pagina<=1) {
+                $pagina=1;
+            }
+        }
+
+        $pagina=limpiar_cadena($pagina);
+        $url="index.php?vista=user_list&page=";
+        $registros=15; # REGISTROS POR PÁGINA #
+        $busqueda="";
+
+        require_once "./php/usuario_lista.php";
+    ?>
+
+    <p class="has-text-right">Mostrando usuarios <strong>1</strong> al <strong>9</strong> de un <strong>total de 9</strong></p>
+
+    <nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">
+        <a class="pagination-previous" href="#">Anterior</a>
+
+        <ul class="pagination-list">
+            <li><a class="pagination-link" href="#">1</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link is-current" href="#">2</a></li>
+            <li><a class="pagination-link" href="#">3</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link" href="#">3</a></li>
+        </ul>
+
+        <a class="pagination-next" href="#">Siguiente</a>
+    </nav>
+
+</div>
