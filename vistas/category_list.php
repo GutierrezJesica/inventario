588f4328398d5e049eb639ee8654c4db4b407e79
Gutierrez Jesica <jesica89@gmail.com>
2025-09-10 11:53:50 -0300

    lista de categorias lista y buscador en proceso

--- vistas/category_list.php
+++ vistas/category_list.php
@@ -1,1 +1,35 @@
-lista de categorias
+<div class="container is-fluid mb-6">
+    <h1 class="title">Categorías</h1>
+    <h2 class="subtitle">Lista de categoría</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+
+<?php # INCORPORACIÓN DE ARCHIVOS PRINCIPALES PARA CARGAR LA LISTA DE CATEGORIA #
+        require_once "./php/main.php";
+
+        # ELIMINAR CATEGORIA #
+        if(isset($_GET['category_id_del'])) {
+            require_once "./php/usuario_eliminar.php";
+        }
+
+        if(!isset($_GET['page'])) {
+            $pagina=1;
+        }else {
+            $pagina=(int)$_GET['page'];
+            if($pagina<=1) {
+                $pagina=1;
+            }
+        }
+
+        $pagina=limpiar_cadena($pagina);
+        $url="index.php?vista=category_list&page=";
+        $registros=10; # REGISTROS POR PÁGINA #
+        $busqueda="";
+
+        # PAGINADOR CATEGORIA #
+        require_once "./php/categoria_lista.php";
+    ?>
+
+
+</div>
